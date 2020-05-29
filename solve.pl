049ab6b72fb552878076c775106e3a39c0cb419a
Adam Benda <adam@adasek.cz>
2020-05-29 20:49:50 +0200

    Do not use same word twice

--- solve.pl
+++ solve.pl
@@ -10,12 +10,26 @@ load_word_masks :-
 load_word_space_fills :-
  consult(word_space_fills).
 
-find_and_output_word_space(WordSpaceName) :-
+% https://stackoverflow.com/a/15865303
+not_used_word(_, []) :- !.
+
+not_used_word(X, [Head|Tail]) :-
+     X \= Head,
+    not_used_word(X, Tail).
+
+find_and_output_word_space(WordSpaceName, UsedWords, WordString) :-
     word_space_fill(Mask, Chars, WordSpaceName),
     word_mask(Mask, Chars, WordString),
+    not_used_word(WordString, UsedWords),
     print(WordSpaceName),write("="),print(WordString),nl.
+
+solve_word_spaces([],_).
+solve_word_spaces([WordSpaceName | Rest], UsedWordsBefore) :-
+    find_and_output_word_space(WordSpaceName, UsedWordsBefore, WordString),
+    solve_word_spaces(Rest, [WordString|UsedWordsBefore]).
 
 
 generate_cross :-
     write("Crossword filling:"),nl,
-    forall(word_space_name(WordSpaceName), find_and_output_word_space(WordSpaceName)).
+    bagof(WordSpaceName, word_space_name(WordSpaceName), WordSpaceNames),
+    solve_word_spaces(WordSpaceNames, []).
