e20a0aabab1ea7d2fd1804a7d22813ab4aec37c8
Adam Benda <adam@adasek.cz>
2020-05-30 23:44:13 +0200

    Write words after they are computed

--- solve.pl
+++ solve.pl
@@ -20,16 +20,23 @@ not_used_word(X, [Head|Tail]) :-
 find_and_output_word_space(WordSpaceName, UsedWords, WordString) :-
     word_space_fill(Mask, Chars, WordSpaceName),
     word_mask(Mask, Chars, WordString),
-    not_used_word(WordString, UsedWords),
-    print(WordSpaceName),write("="),print(WordString),nl.
+    not_used_word(WordString, UsedWords).
+    %print(WordSpaceName),write("="),print(WordString),nl.
 
-solve_word_spaces([],_).
-solve_word_spaces([WordSpaceName | Rest], UsedWordsBefore) :-
+solve_word_spaces([],X,X).
+solve_word_spaces([WordSpaceName | Rest], UsedWordsBefore, UsedWords) :-
     find_and_output_word_space(WordSpaceName, UsedWordsBefore, WordString),
-    solve_word_spaces(Rest, [WordString|UsedWordsBefore]).
+    solve_word_spaces(Rest, [WordString|UsedWordsBefore], UsedWords).
+
+print_word_spaces([],[]).
+print_word_spaces([WordSpaceName|Rest1],[UsedWord|Rest2]) :-
+    print(WordSpaceName),write(":"),print(UsedWord),nl,
+    print_word_spaces(Rest1,Rest2).
 
 
 generate_cross :-
-    write("Crossword filling:"),nl,
+    write("..."),nl,
     bagof(WordSpaceName, word_space_name(WordSpaceName), WordSpaceNames),
-    solve_word_spaces(WordSpaceNames, []).
+    solve_word_spaces(WordSpaceNames, [], UsedWords),
+    write("Crossword filling:"),nl,
+    print_word_spaces(WordSpaceNames, UsedWords).
