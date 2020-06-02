42d9dbdf623ea4ec8cc8f7d2ac20bb72a7c67093
Adam Benda <adam@adasek.cz>
2020-06-02 19:41:08 +0200

    Experiment1 captures output

--- crossword/experiments/e1.py
+++ crossword/experiments/e1.py
@@ -9,15 +9,15 @@ import subprocess
 class Experiment1:
   def __init__(self, dictionary_filename):
     # modify wordlist so that it contains only words of 8
-    fp = open(dictionary_filename)
-    lines = fp.readlines()
-    words_all = [line.split('/')[0].lower().strip() for line in lines]
-    in_words = [word for word in words_all if len(word) == 8]
+    with open(dictionary_filename) as fp:
+      lines = fp.readlines()
+      words_all = [line.split('/')[0].lower().strip() for line in lines]
+      in_words = [word for word in words_all if len(word) == 8]
     with open('wordlist.dat', 'w') as wordlist_fp:
       for word in in_words:
         print(word, file=wordlist_fp)
 
-    for number_of_words in range(1, 200):
+    for number_of_words in range(8, 200):
       with open('crossword.dat', 'w') as crossword_fp:
         for line_number in range(0, number_of_words):
           print("X"*line_number, sep="", end="", file=crossword_fp)
@@ -43,9 +43,17 @@ class Experiment1:
       exporter.export_all(words, word_spaces, possible_masks, words_by_masks)
 
       t0 = time.time()
-      process = subprocess.run(['prolog', '../solve.pl'], cwd='prolog_output', capture_output=True )
+      process = subprocess.run(['prolog', '../solve.pl'], cwd='prolog_output', capture_output=True)
+      output = process.stdout
+      # Parse output: first two lines contain load_time and compute_time
+      # other lines contains values of WordSpaces
+      try:
+        output_dict = {line.split(":")[0]: line.split(":")[1] for line in output.decode('utf-8').split("\n") if line.strip() != ''}
+      except IndexError:
+        print(output.decode('utf-8'))
+        raise Exception("Unexpected prolog program output format")
 
       t1 = time.time()
 
-      print(f"{number_of_words},{t1-t0},{process.returncode}")
+      print(f"{number_of_words},{t1-t0},{output_dict['load_time']},{output_dict['compute_time']},{process.returncode}")
 
---
+++ experiment.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from crossword.experiments import Experiment1
+
+
+Experiment1("./input/Czech.dic")
