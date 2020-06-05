dc2a6c696ab367617be788752201fd3751372658
Adam Benda <adam@adasek.cz>
2020-06-05 22:49:06 +0200

    Cross test

--- tests/crossword/objects/__init__.py
+++ tests/crossword/objects/__init__.py
@@ -1,5 +1,2 @@
-from .cross import Cross
-from .mask import Mask
-from .word import Word
-from .charlist import CharList
-from .word_space import WordSpace
+from .test_cross import CrossTest
+
---
+++ tests/crossword/objects/test_cross.py
@@ -0,0 +1,29 @@
+
+import unittest
+import pytest
+from crossword.objects import Cross
+from crossword.objects import WordSpace
+
+class CrossTest(unittest.TestCase):
+
+    def test_init(self):
+        word_space1 = WordSpace((2, 1), 3, 'vertical')
+        word_space2 = WordSpace((1, 2), 3, 'horizontal')
+        cross = Cross(word_space1, word_space2)
+
+        assert hasattr(cross, "word_space_horizontal")
+        assert hasattr(cross, "word_space_vertical")
+        assert cross.word_space_horizontal == word_space2
+        assert cross.word_space_vertical == word_space1
+
+        assert cross.index_in_horizontal == 1
+        assert cross.index_in_vertical == 1
+
+        assert cross.coordinates == (2, 2)
+
+    def test_id(self):
+        word_space1 = WordSpace((2, 1), 3, 'vertical')
+        word_space2 = WordSpace((1, 2), 3, 'horizontal')
+        cross = Cross(word_space1, word_space2)
+
+        assert cross.id() == "C_2_2"
