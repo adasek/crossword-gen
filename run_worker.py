9f44f6cddbe8d4148c35a3c080386014723a8c91
Adam Benda <adam@adasek.cz>
2021-05-06 14:56:58 +0200

    worker wrapper err handling

--- run_worker.py
+++ run_worker.py
@@ -1,4 +1,5 @@
 from config import ENV
+import faktory
 from faktory import Worker
 import time
 import logging
@@ -80,12 +81,12 @@ def generate_crossword(crossword_task):
     max_crossword = None
     for i in range(100):
         start = time.perf_counter()
-        word_spaces = solver.solve(crossword, word_list, randomize=0.05, assign_first_word=True, max_failed_words=10)
+        word_spaces = solver.solve(crossword, word_list, randomize=0.05, assign_first_word=True, max_failed_words=1000)
         print(f"Score: {solver.score} in {round(-start + (time.perf_counter()), 2)}s")
         # if not solver.solution_found:
         #    print(crossword)
-        if word_spaces is not None and word_spaces is not False and solver.score > max_score:
-            max_score = solver.score
+        if crossword.is_success() and crossword.evaluate_score() > max_score:
+            max_score = crossword.evaluate_score()
             max_crossword = crossword.get_copy()
         crossword.reset()
     # Send the crossword back
@@ -108,13 +109,22 @@ def generate_crossword(crossword_task):
 # input_json = json.loads('{"id":9,"user_id":1,"grid_id":8,"categorization_preference_id":9,"status":"created","createdAt":"2021-04-19T16:01:41.845Z","updatedAt":"2021-04-19T16:31:55.864Z","Grid":{"image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAMCAIAAADUCbv3AAAACXBIWXMAAAPoAAAD6AG1e1JrAAAANElEQVQYlWNgIAj+YwMMYHHs0gwwQSzSDEgiOA2HyqFZj4WLZiB2u3Gpw66PLGksjscjDQA4WgEOOngFMQAAAABJRU5ErkJggg==","id":8,"user_id":1,"width":10,"height":12,"bitmap":"XXXXXXXXXXX       X X      X  X     X   X        XXX   XX   X X    X  X  X X    X    X    X    X    X         X         ","createdAt":"2021-04-18T11:15:56.871Z","updatedAt":"2021-04-18T11:15:56.871Z"},"CategorizationPreference":{"id":9,"user_id":1,"categorization_type":1,"value":{"BIO":"-1","CHE":"1","ECO":"1","EDU":"-1","GEO":"0.3","HIS":"-1","ICT":"1","INF":"0","LAN":"-1","LAW":"-1","LIF":"-1","MAT":"1","MED":"-1","MIX":"-1","PHI":"-1","PHY":"1","POL":"-1","PSY":"-1","REC":"-1","SCT":"-1","SOC":"-1","SPO":"-1","TEC":"1","THE":"-1"},"createdAt":"2021-04-19T16:01:41.829Z","updatedAt":"2021-04-19T16:01:41.829Z"}}')
 # generate_crossword(input_json)
 
-w = Worker(queues=['default'], concurrency=1, faktory=ENV['FAKTORY_URL'])
-w.register('CrosswordTask', generate_crossword)
-
+worker = None
 while True:
     try:
-        w.run()
+        if worker is None:
+            worker = Worker(queues=['default'], concurrency=1, faktory=ENV['FAKTORY_URL'])
+            worker.register('CrosswordTask', generate_crossword)
+        try:
+            worker.run()
+        except ConnectionRefusedError:
+            time.sleep(60)
+            continue
     except json.decoder.JSONDecodeError:
         # todo: log this
-        print(f"JSONDecodeError, probably server heartbeat missed")
+        print(f"probably server heartbeat missed")
         time.sleep(30)
+    except faktory.exceptions.FaktoryConnectionResetError:
+        worker = None
+        time.sleep(60)
+        # try to revive the worker
