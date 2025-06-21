fcde4f83ed16ac037f82e700fa0c31f4d478dc8c
Adam Benda <adam@adasek.cz>
2025-06-21 20:27:07 +0200

    Refactor: Isolate get_candidate_char_dict

--- crossword/objects/word_space.py
+++ crossword/objects/word_space.py
@@ -130,29 +130,13 @@ class WordSpace:
         # mask ...X.
         candidate_char_dict_array = []
         for cross in unbounded_crosses:
-            other_wordspace = cross.other(self)
-            other_wordspace_mask, other_wordspace_chars = other_wordspace.mask_current()
-            base_set = word_list.words_indices(other_wordspace_mask, other_wordspace_chars)
-            if other_wordspace_mask.is_fully_bound():
-                candidate_char_dict_array.append({cross.bound_value(): 1})
-                continue
-            other_wordspace_index = other_wordspace.index_of_cross(cross)
-            mask_list = [False] * other_wordspace.length
-            mask_list[other_wordspace_index] = True
-            one_mask = Mask(mask_list)
-
-            candidate_chars = []
-            for char in word_list.alphabet:
-                suitable_words_index = base_set.intersection(
-                    word_list.words_indices(one_mask, CharList([char]))).difference(
-                    other_wordspace.failed_words_index_set)
-                words_count = len(suitable_words_index)
-                if words_count > 0:
-                    candidate_chars.append({'char': char, 'count': words_count})
-            if len(candidate_chars) == 0:
-                return None
-            sorted_candidate_char_records = sorted(candidate_chars, key=lambda rec: rec['count'], reverse=True)
-            candidate_char_dict_array.append({obj['char']: obj['count'] for obj in sorted_candidate_char_records})
+            if cross.is_half_bound():
+                # The other side already bounded this
+                candidate_char_dict = {cross.bound_value(): 1}
+            else:
+                other_wordspace = cross.other(self)
+                candidate_char_dict = other_wordspace.get_candidate_char_dict(word_list, cross)
+            candidate_char_dict_array.append(candidate_char_dict)
 
         words_dataframe = self.bindable(word_list)
 
@@ -203,6 +187,22 @@ class WordSpace:
         else:
             # print(f"find_best_option: None")
             return None
+
+    # Creates a map {'a':5, ...} stating the number of words if the given cross is bound to such char.
+    # The numbers are according to the current binding
+    def get_candidate_char_dict(self, word_list: WordList, cross: Cross):
+        suitable_words = word_list.words_indices(*self.mask_current()).difference(self.failed_words_index_set)
+
+        candidate_chars = {}
+        for char in word_list.alphabet:
+            cross_index = self.index_of_cross(cross)
+            mask_list = [(i == cross_index) for i in range(self.length)]
+            one_mask = Mask(mask_list)
+            words_with_that_char = suitable_words.intersection(word_list.words_indices(one_mask, [char]))
+            words_count = len(words_with_that_char)
+            if words_count > 0:
+                candidate_chars[char] = words_count
+        return candidate_chars
 
     # Returns set of tuples - positions that this words goes through
     def spaces(self):
