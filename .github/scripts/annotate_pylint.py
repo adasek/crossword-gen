439639d668c8f8658a2451b72e8babfc2f209343
Adam Benda <adam@adasek.cz>
2025-07-03 17:37:17 +0200

    Github actions (4)

---
+++ .github/scripts/annotate_pylint.py
@@ -0,0 +1,17 @@
+import json
+import sys
+
+with open("pylint-report.json") as f:
+    data = json.load(f)
+
+levels = {"error": "error", "warning": "warning", "refactor": "warning", "convention": "notice"}
+
+for msg in data:
+    path = msg.get("path")
+    line = msg.get("line")
+    col = msg.get("column")
+    msg_id = msg.get("symbol")
+    message = msg.get("message")
+    level = levels.get(msg.get("type"), "warning")
+
+    print(f"::{level} file={path},line={line},col={col}::{msg_id}: {message}")
